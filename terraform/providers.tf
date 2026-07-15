b615046df41a1ce1e1e3eccf8e03ac9555f8a198
immajor <immajor25@gmail.com>
2026-07-15 14:53:52 +0200

    Fixing terraform variables and syntax errors

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,16 @@
 terraform {
-    required_version = ">= 1.5"
+  required_version = ">= 1.5"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
 }
 
 provider "kubernetes" {
